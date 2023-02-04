7cdafcd6dcac83db4b40e92ccb585347cc66fd84
CameronAron <cameron.aron04@gmail.com>
2023-02-04 00:05:46 -0600

    Lever actually works now

--- Assets/Ethan/Lever.cs
+++ Assets/Ethan/Lever.cs
@@ -6,15 +6,20 @@ public class Lever : MonoBehaviour
 {
     // Item you want to dissapear
     public GameObject blockade;
+    public bool isPlayerByLever;
+
 
     void OnTriggerStay2D(Collider2D obj){
         // Checks if player is standing inside the lever box collider
         if (obj.CompareTag("Player")){
             // If the player presses E while standing inside the box collider
             // it deactivates whatever GameObjects are set to blockade
-            if(Input.GetKeyDown(KeyCode.E) == true){
+
+            isPlayerByLever = true;
+
+            if (Input.GetKey(KeyCode.E)){
                 blockade.SetActive(false);
             }
         }
-    }
+    } 
 }
