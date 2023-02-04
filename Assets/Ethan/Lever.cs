7aea0bc2390bff6396433dc9fb8d651e102540e7
Atlas1004 <Schneideea04@uww.edu>
2023-02-03 22:36:30 -0600

    Lever shit and lvl 1 layout

---
+++ Assets/Ethan/Lever.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lever : MonoBehaviour
+{
+    // Item you want to dissapear
+    public GameObject blockade;
+
+    void OnTriggerStay2D(Collider2D obj){
+        // Checks if player is standing inside the lever box collider
+        if (obj.CompareTag("Player")){
+            // If the player presses E while standing inside the box collider
+            // it deactivates whatever GameObjects are set to blockade
+            if(Input.GetKeyDown(KeyCode.E) == true){
+                blockade.SetActive(false);
+            }
+        }
+    }
+}
