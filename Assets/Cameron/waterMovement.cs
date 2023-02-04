6ed33dc02a404f7c4786813a40d8e5d74d7b81e3
CameronAron <cameron.aron04@gmail.com>
2023-02-04 16:20:38 -0600

    :(

--- Assets/Cameron/waterMovement.cs
+++ Assets/Cameron/waterMovement.cs
@@ -6,6 +6,11 @@ public class waterMovement : MonoBehaviour
 {
     public float thrustSpeed = 1f;
     public float turnSpeed = 1f;
+    public float dashSpeed = 5f;
+    public float dashCooldown;
+    public bool dashAvailable = true;
+
+    public bool inCircle = true;
 
     private Rigidbody2D _rigidbody;
     private bool _thrusting;
@@ -33,6 +38,30 @@ public class waterMovement : MonoBehaviour
         else
         {
             _turnDirection = 0f;
+        }   
+    }
+
+    private void OnTriggerExit2D(Collider2D obj)
+    {
+        if (obj.CompareTag("Swimable"))
+        {
+            inCircle = false;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D obj)
+    {
+        if (obj.CompareTag("Swimable"))
+        {
+            inCircle = true;
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D obj)
+    {
+        if (obj.CompareTag("Swimable"))
+        {
+            inCircle = true;
         }
     }
 
@@ -43,9 +72,32 @@ public class waterMovement : MonoBehaviour
             _rigidbody.AddForce(this.transform.right * this.thrustSpeed);
         }
 
+        if (!inCircle)
+        {
+            _rigidbody.AddForce(this.transform.right * -this.thrustSpeed * 8);
+        }
+
         if (_turnDirection != 0f)
         {
             _rigidbody.AddTorque(_turnDirection * this.turnSpeed);
+        }
+
+        if (Input.GetKey(KeyCode.Space) && dashAvailable)
+        {
+            //Debug.Log("pressed space");
+            _rigidbody.AddForce(this.transform.right * dashSpeed);
+            dashCooldown = 0;
+            dashAvailable = false;
+        }
+
+        if (!dashAvailable)
+        {
+            dashCooldown += Time.deltaTime;
+        }
+
+        if (dashCooldown > 1)
+        {
+            dashAvailable = true;
         }
     }
 }
