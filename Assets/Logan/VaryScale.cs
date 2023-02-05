99ed8b913e5bfbbc26ebffd6672a7cabb942f026
lmueller47 <92188368+lmueller47@users.noreply.github.com>
2023-02-05 02:16:58 -0600

    fixes

---
+++ Assets/Logan/VaryScale.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VaryScale : MonoBehaviour
+{
+    public bool playing = false;
+    private void Update()
+    {
+        if (!playing)
+        {
+            playing = true;
+            StartCoroutine(Scaler());
+        }
+    }
+
+    IEnumerator Scaler()
+    {
+        transform.localScale = new Vector3(.33f, 0.33f, 0);
+        yield return new WaitForSeconds(1f);
+        transform.localScale = new Vector3(1, 1, 0);
+        yield return new WaitForSeconds(1f);
+        transform.localScale = new Vector3(.33f, 0.33f, 0);
+        playing = false;
+    }
+}
