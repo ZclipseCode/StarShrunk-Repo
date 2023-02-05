370f3411e24da5b706f934a3f264c6af8b925f5c
CameronAron <cameron.aron04@gmail.com>
2023-02-04 18:59:45 -0600

    :|

---
+++ Assets/Cameron/CheatCode.cs
@@ -0,0 +1,108 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheatCode : MonoBehaviour
+{
+    private bool typedB = false;
+    private bool typedR = false;
+    private bool typedI = false;
+    private bool typedA = false;
+    private bool typedN = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            if (!typedB)
+            {
+                typedB = true;
+            }
+            else if (typedB)
+            {
+                typedB = false;
+                typedR = false;
+                typedI = false;
+                typedA = false;
+                typedN = false;
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.R))
+        {
+            if (!typedR)
+            {
+                typedR = true;
+            }
+            else if (typedR)
+            {
+                typedB = false;
+                typedR = false;
+                typedI = false;
+                typedA = false;
+                typedN = false;
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.I))
+        {
+            if (!typedI)
+            {
+                typedI = true;
+            }
+            else if (typedI)
+            {
+                typedB = false;
+                typedR = false;
+                typedI = false;
+                typedA = false;
+                typedN = false;
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.A))
+        {
+            if (!typedA)
+            {
+                typedA = true;
+            }
+            else if (typedA)
+            {
+                typedB = false;
+                typedR = false;
+                typedI = false;
+                typedA = false;
+                typedN = false;
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.N))
+        {
+            if (!typedN)
+            {
+                typedN = true;
+            }
+            else if (typedN)
+            {
+                typedB = false;
+                typedR = false;
+                typedI = false;
+                typedA = false;
+                typedN = false;
+            }
+        }
+
+        if (typedN)
+        {
+            Debug.Log("I love Brian");
+            typedB = false;
+            typedR = false;
+            typedI = false;
+            typedA = false;
+            typedN = false;
+        }
+    }
+}
