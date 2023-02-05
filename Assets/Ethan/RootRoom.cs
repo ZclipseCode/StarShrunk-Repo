a59a11524f5a35f612668d40375f26660eae3612
lmueller47 <92188368+lmueller47@users.noreply.github.com>
2023-02-05 12:04:09 -0600

    lvl 1 transition

--- Assets/Ethan/RootRoom.cs
+++ Assets/Ethan/RootRoom.cs
@@ -4,8 +4,13 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class RootRoom : MonoBehaviour{
-    public GameObject player, openState, closedState, keyUIObj;
+    public GameObject player, openState, closedState, keyUIObj, beholder;
     public bool hasKey = false, isClosed = true, isPlayerNearRoom;
+
+    private void Start()
+    {
+        beholder.SetActive(false);
+    }
     void Update(){
         hasKey = keyUIObj.activeInHierarchy;
         if(isPlayerNearRoom && Input.GetKeyDown(KeyCode.E)) {
@@ -15,7 +20,8 @@ public class RootRoom : MonoBehaviour{
                 isClosed = false;
             }
             else if (!isClosed) {
-                SceneManager.LoadScene("Cam LVL2");
+                //SceneManager.LoadScene("Cam LVL2");
+                StartCoroutine(Behold());
             }
         }
     }
@@ -26,5 +32,16 @@ public class RootRoom : MonoBehaviour{
     }
     private void OnTriggerExit2D(Collider2D obj) {
         isPlayerNearRoom = false;
+    }
+
+    IEnumerator Behold()
+    {
+        beholder.SetActive(true);
+        RaiseBeholder.start = true;
+        ScreenShake.start = true;
+        yield return new WaitForSeconds(5f);
+        RaiseBeholder.start = false;
+        beholder.SetActive(false);
+        SceneManager.LoadScene("Cam LVL2");
     }
 }
